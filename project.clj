53cd8e8b50832c91e81ce5411e55b2610b7fdff9
Kaushik Deb <kdeb@secureworks.com>
2018-06-11 18:44:55 +0100

    jetty server test: tested how to create jetty server, also tested with uberjar that it works from any host

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http-lite "0.3.0"]]
+                 [clj-http-lite "0.3.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
   :main ^:skip-aot kaushikdeb.test-clojure.test1
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kaushikdeb/test_clojure/test1.clj
+++ src/kaushikdeb/test_clojure/test1.clj
@@ -1,5 +1,6 @@
 (ns kaushikdeb.test-clojure.test1
-  (:require [clj-http.lite.client :as client])
+  (:require [clj-http.lite.client :as client]
+            [ring.adapter.jetty :as jetty])
   (:gen-class))
 
 (defn test-http
@@ -10,9 +11,26 @@
     (client/get url)))
 
 
-(defn -main
+(defn -main1
   "entry point for the app"
   [& args];; expects the URL to be passed in the command line
   (println "Hello, World!")
   (when (and (not (nil? args)) (> (count args) 0));; Go to the next line only if args is passed
     (println "Response status from http client call: " (get (test-http (first args)) :status))))
+
+(defn now [] (java.util.Date.))
+
+(defn current-time [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (str (now))})
+
+(defn -main
+  "entry point 2 to test jetty server"
+  [& args];; Not used
+  (def my-port 3000)
+  (when (and (not (nil? args)) (> (count args) 0))
+    (def my-port
+      (try (Integer/parseInt (first args))
+           (catch Exception exception my-port))))
+  (jetty/run-jetty current-time {:port my-port}))
